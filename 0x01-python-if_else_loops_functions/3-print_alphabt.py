7c1583eee939bf2ef1eb38880af793ca23c7fbf2
luche3002 <amukhobukwame@gmail.com>
2025-01-23 10:08:21 +0300

    Task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for char in range(97, 123):
-        print("{}".format(chr(char)), end="")
+# Author - Tolulope Fakunle
+for letter in range(97, 123):
+            if chr(letter) != 'q' and chr(letter) != 'e':
+                            print("{}".format(chr(letter)), end="")
