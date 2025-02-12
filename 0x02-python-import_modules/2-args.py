9e84523433eb6cee783f25ca5d54aa958d4650be
luche3002 <amukhobukwame@gmail.com>
2025-02-12 11:54:23 +0300

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+
+import sys
+
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+    argv = sys.argv[1:]
+
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
+
+    for i, arg in enumerate(argv, 1):
+        print("{}: {}".format(i, arg))
