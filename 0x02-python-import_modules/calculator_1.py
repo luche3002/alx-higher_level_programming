036b1733429b3c5dd6218e25b01bd63acbacccea
luche3002 <amukhobukwame@gmail.com>
2025-02-12 14:01:37 +0300

    Task 1

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,51 +1,51 @@
 #!/usr/bin/python3
 def add(a, b):
-        """My addition function
+    """My addition function
 
-        Args:
-            a: first integer
-            b: second integer
+    Args:
+        a: first integer
+        b: second integer
 
-        Returns:
-            The return value. a + b
-        """
-        return (a + b)
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
 
 
 def sub(a, b):
-        """My subtraction function
+    """My subtraction function
 
-        Args:
-            a: first integer
-            b: second integer
+    Args:
+        a: first integer
+        b: second integer
 
-        Returns:
-            The return value. a - b
-        """
-        return (a - b)
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
 
 
 def mul(a, b):
-        """My multiplication function
+    """My multiplication function
 
-        Args:
-            a: first integer
-            b: second integer
+    Args:
+        a: first integer
+        b: second integer
 
-        Returns:
-            The return value. a * b
-        """
-        return (a * b)
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
 
 
 def div(a, b):
-        """My division function
+    """My division function
 
-        Args:
-            a: first integer
-            b: second integer
+    Args:
+        a: first integer
+        b: second integer
 
-        Returns:
-            The return value. a / b
-        """
-        return int(a / b)
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
